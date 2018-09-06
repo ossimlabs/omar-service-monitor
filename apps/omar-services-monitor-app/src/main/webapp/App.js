fbe5c19ced6ade99639ba347fde7f7b00d4a64c5
melbsurfer <melbsurfer@gmail.com>
2018-09-06 09:55:35 -0400

    Starting to add some error handling.

--- apps/omar-services-monitor-app/src/main/webapp/App.js
+++ apps/omar-services-monitor-app/src/main/webapp/App.js
@@ -3,27 +3,27 @@ import ErrorBoundary from "./ErrorBoundary";
 import Deployment from "./Deployment";
 
 class App extends Component {
-
   render() {
-
     return (
       <ErrorBoundary>
         <nav>
           <div className="nav-wrapper black">
             <div className="">
-              <a href="#" className="brand-logo center">O2 Monitor</a>
+              <a href="#" className="brand-logo center">
+                O2 Services Monitor
+              </a>
             </div>
           </div>
         </nav>
-        {
-          // AppParams is a global passed down from application.yml
-          // through the .gsp
-          AppParams.params.deployments.map((deployment, i) => {
-            return (<div className="row"key={i}>
+        {// AppParams is a global passed down from application.yml
+        // through the .gsp
+        AppParams.params.deployments.map((deployment, i) => {
+          return (
+            <div className="row" key={i}>
               <Deployment server={deployment.url} />
-            </div>)
-          })
-        }
+            </div>
+          );
+        })}
       </ErrorBoundary>
     );
   }
--- apps/omar-services-monitor-app/src/main/webapp/ServiceMonitor.js
+++ apps/omar-services-monitor-app/src/main/webapp/ServiceMonitor.js
@@ -13,32 +13,29 @@ class ServiceMonitor extends Component {
     setInterval(() => {
       const appName = this.props.app.name.toLowerCase();
       //console.log('appName: ', appName);
-      fetch(
-        `${this.props.server}/omar-eureka-server/eureka/vips/${appName}`,
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-          }
+      fetch(`${this.props.server}/omar-eureka-server/eureka/vips/${appName}`, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
         }
-      )
-      .then(function(response) {
-        return response.json();
       })
-      .then(instanceJson => {
-        //console.log('instanceJson', instanceJson);
-        const instances = instanceJson.applications.application[0].instance;
+        .then(function(response) {
+          return response.json();
+        })
+        .then(instanceJson => {
+          //console.log('instanceJson', instanceJson);
+          const instances = instanceJson.applications.application[0].instance;
 
-        this.setState({ instances: instances });
-        //console.log('State in the ServiceMonitor', this.state);
-      })
-      .catch(error => console.error("Error connecting to server: " + error));
-    }, 1500);
+          this.setState({ instances: instances });
+          //console.log('State in the ServiceMonitor', this.state);
+        })
+        .catch(error => console.error(`[Fetch Services Error] connecting to ${this.props.server} with ${error}`));
+    }, 7500);
   };
 
-  getVersion = (version) => {
-    this.setState({version});
-  }
+  getVersion = version => {
+    this.setState({ version });
+  };
 
   componentDidMount() {
     //console.log('ServiceMonitor Props: ', this.props);
@@ -47,23 +44,30 @@ class ServiceMonitor extends Component {
 
   render() {
     if (this.state.instances.length === 0) {
-      return  <p>Loading service...</p>
+      return <p>Loading service...</p>;
     }
 
     return (
       <React.Fragment>
-        <p className="service-name">{this.props.app.name} {this.state.version}</p>
-          {this.state.instances.map((instance, i) => {
-            return (
-              <div key={i}>
-                <Instance
-                  data={instance}
-                  server={this.props.server}
-                  getVersion={this.getVersion} />
-              </div>
-            );
-          })}
-          <img className="right" src="https://jenkins.ossim.io/buildStatus/icon?job=omar-oms-dev" alt=""/>
+        <p className="service-name">
+          {this.props.app.name} {this.state.version}
+        </p>
+        {this.state.instances.map((instance, i) => {
+          return (
+            <div key={i}>
+              <Instance
+                data={instance}
+                server={this.props.server}
+                getVersion={this.getVersion}
+              />
+            </div>
+          );
+        })}
+        {/* <img
+          className="right"
+          src="https://jenkins.ossim.io/buildStatus/icon?job=omar-oms-dev"
+          alt=""
+        /> */}
       </React.Fragment>
     );
   }
