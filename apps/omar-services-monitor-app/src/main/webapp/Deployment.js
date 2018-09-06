1db5d12f34cc5438517612cc4af5a118446cdb9d
melbsurfer <melbsurfer@gmail.com>
2018-09-06 09:55:35 -0400

    Starting to add some error handling.

--- apps/omar-services-monitor-app/src/main/webapp/Deployment.js
+++ apps/omar-services-monitor-app/src/main/webapp/Deployment.js
@@ -1,30 +1,38 @@
 import React, { Component } from "react";
-import ErrorBoundary from "./ErrorBoundary";
 import ServiceMonitor from "./ServiceMonitor";
 
 //import { SERVER_URL, CLIENT_VERSION, REACT_VERSION } from "./config";
 import "whatwg-fetch";
 
 class Deployment extends Component {
-
   state = {
     deploymentInfo: [],
-    appsInfo: []
+    appsInfo: [],
+    error: false
   };
 
   fetchDeployment = () => {
     fetch(`${this.props.server}/omar-eureka-server/env`)
       .then(function(response) {
+        // if (!response.ok) {
+        //   throw Error(response.statusText);
+        // }
         return response.json();
       })
       .then(deploymentJson => {
         //console.log("deploymentJson: ", deploymentJson['configService:file:/home/omar/configs/application.yml']);
-        const deployment = deploymentJson['configService:file:/home/omar/configs/application.yml'];
+        const deployment =
+          deploymentJson[
+            "configService:file:/home/omar/configs/application.yml"
+          ];
         //console.log("deployment: ", deployment);
         this.setState({ deploymentInfo: deployment });
       })
-      .catch(error => console.error("Error connecting to server: " + error));
-  }
+      .catch(error => {
+        console.error(`[Fetch Deployments Error] connecting to ${this.props.server} with ${error}`);
+        this.setState({ error: true });
+      });
+  };
 
   fetchApps = () => {
     setInterval(() => {
@@ -35,15 +43,15 @@ class Deployment extends Component {
           "Content-Type": "application/json"
         }
       })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(eurekaJson => {
-        const Apps = eurekaJson;
-        this.setState({ appsInfo: Apps });
-      })
-      .catch(error => console.error("Error connecting to server: " + error));
-    }, 1500);
+        .then(function(response) {
+          return response.json();
+        })
+        .then(eurekaJson => {
+          const Apps = eurekaJson;
+          this.setState({ appsInfo: Apps });
+        })
+        .catch(error => console.error(`[Fetch Apps Error] connecting to ${this.props.server} with ${error}`));
+    }, 5000);
   };
 
   componentDidMount() {
@@ -54,19 +62,26 @@ class Deployment extends Component {
   }
 
   render() {
-    if (this.state.appsInfo.length === 0) {
-      return (<div className="progress">
-                <div className="indeterminate"></div>
-              </div>);
+    if (this.state.appsInfo.length === 0 && this.state.error === false) {
+      return (
+        <div className="progress">
+          <div className="indeterminate" />
+        </div>
+      );
+    }
+    else if (this.state.appsInfo.length === 0 && this.state.error === true) {
+      return (<div className="deployment"><p className="chip red lighten-1"><span className="white-text">There was an error fetching the details of the {this.props.server} deployment</span></p></div>)
     }
 
     return (
       <div className="deployment">
         <section>
-          {/* <p className="deployment-url">Deployment: {this.props.server}</p> */}
           <a href={this.props.server}>{this.props.server}</a>
-          <p className="deployment-version">Version: {this.state.deploymentInfo.releaseNumber} ({this.state.deploymentInfo.releaseName})</p>
-          <div className="">
+          <p className="deployment-version">
+          Version: {this.state.deploymentInfo.releaseNumber} (
+            {this.state.deploymentInfo.releaseName})
+          </p>
+
             {this.state.appsInfo.applications.application.map((app, i) => {
               return (
                 <div className="col s2 z-depth-2 service" key={i}>
@@ -74,12 +89,11 @@ class Deployment extends Component {
                 </div>
               );
             })}
-          </div>
+
         </section>
       </div>
     );
   }
-
 }
 
 export default Deployment;
