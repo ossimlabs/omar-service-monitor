930839eeb5d4a4e4b1f2a18ec2c7fe26fcb8cd1a
melbsurfer <melbsurfer@gmail.com>
2018-09-07 12:18:55 -0400

    Help me Scottie you are my only hope

--- apps/omar-services-monitor-app/src/main/webapp/ServiceMonitor.js
+++ apps/omar-services-monitor-app/src/main/webapp/ServiceMonitor.js
@@ -10,27 +10,56 @@ class ServiceMonitor extends Component {
   };
 
   fetchServices = () => {
-    setInterval(() => {
-      const appName = this.props.app.name.toLowerCase();
-      //console.log('appName: ', appName);
-      fetch(`${this.props.server}/omar-eureka-server/eureka/vips/${appName}`, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        }
-      })
-        .then(function(response) {
-          return response.json();
-        })
-        .then(instanceJson => {
-          //console.log('instanceJson', instanceJson);
-          const instances = instanceJson.applications.application[0].instance;
+    // Need to set a variable for this so that we can still access
+    // 'this' for props and state inside the setInterval callback
+    // function
+    let _this = this;
 
-          this.setState({ instances: instances });
-          //console.log('State in the ServiceMonitor', this.state);
-        })
-        .catch(error => console.error(`[Fetch Services Error] connecting to ${this.props.server} with ${error}`));
-    }, 7500);
+    let servicesTimer = setInterval(
+      function fetchServicesData() {
+        console.log("################# services ############## ");
+
+        const appName = _this.props.app.name.toLowerCase();
+        //console.log('appName: ', appName);
+        fetch(
+          `${_this.props.server}/omar-eureka-server/eureka/vips/${appName}`,
+          {
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json"
+            }
+          }
+        )
+          .then(function(response) {
+            return response.json();
+          })
+          .then(instanceJson => {
+            console.log("instanceJson", instanceJson);
+            const instances = instanceJson.applications.application[0].instance;
+
+            _this.setState({ instances: instances });
+            //console.log('State in the ServiceMonitor', this.state);
+          })
+          .catch(error =>
+            console.error(
+              `[Fetch Services Error] connecting to ${
+                _this.props.server
+              } with ${error}`
+            )
+          );
+
+        // Clear the initial 3.5 second pole interval
+        clearInterval(servicesTimer);
+
+        // Set a new pole interval to use what is passed in from the app configuration.
+        // The default from the configuration is every 30 seconds.
+        servicesTimer = setInterval(
+          fetchServicesData,
+          AppParams.params.servicesPoleTime
+        );
+      }.bind(this),
+      3500
+    ); // Intial pole for deployments is after 3.5 seconds
   };
 
   getVersion = version => {
@@ -44,7 +73,7 @@ class ServiceMonitor extends Component {
 
   render() {
     if (this.state.instances.length === 0) {
-      return <p>Loading service...</p>;
+      return <div className="chip center">Fetching service info...</div>;
     }
 
     return (
