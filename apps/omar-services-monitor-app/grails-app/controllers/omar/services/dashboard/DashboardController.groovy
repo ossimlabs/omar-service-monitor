a5f9e9d94db1f5ca496aa93c820691aef87104a0
melbsurfer <melbsurfer@gmail.com>
2018-10-01 11:38:35 -0400

    Refactoring to fix yml list issue with config-server

--- apps/omar-services-monitor-app/grails-app/controllers/omar/services/dashboard/DashboardController.groovy
+++ apps/omar-services-monitor-app/grails-app/controllers/omar/services/dashboard/DashboardController.groovy
@@ -2,11 +2,16 @@ package omar.services.dashboard
 
 class DashboardController {
 
+    DeploymentsConfig deploymentsConfig
+
     def index() {
+
+      //println 'deploymentsConfig: ' + deploymentsConfig
 
       [
         clientConfig: [
-          params: grailsApplication.config.omar.app
+          params: grailsApplication.config.omar.app,
+          deployments: deploymentsConfig.deployments
         ]
       ]
 
