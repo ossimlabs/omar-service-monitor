9de8f280f16470f4bcf0838cf011e4b51ae4a321
melbsurfer <melbsurfer@gmail.com>
2020-05-20 17:03:14 -0400

    Added proxy for Jenkins API calls

--- apps/omar-services-monitor-app/grails-app/controllers/omar/services/dashboard/DashboardController.groovy
+++ apps/omar-services-monitor-app/grails-app/controllers/omar/services/dashboard/DashboardController.groovy
@@ -1,12 +1,18 @@
 package omar.services.dashboard
 
+import org.springframework.beans.factory.annotation.Value;
+
 class DashboardController {
 
     DeploymentsConfig deploymentsConfig
 
-    def index() {
+    @Value('${jenkins.url}')
+    String jenkinsUrl
 
-      //println 'deploymentsConfig: ' + deploymentsConfig
+    @Value('${jenkins.auth}')
+    String jenkinsAuth
+
+    def index() {
 
       [
         clientConfig: [
@@ -14,6 +20,36 @@ class DashboardController {
           deployments: deploymentsConfig.deployments
         ]
       ]
+
+    }
+
+    def proxy() {
+
+      // TODO: Refactor, and use REST Client for POST
+      def cmd = ['curl', '-LX', 'POST', jenkinsUrl, '--user',
+        jenkinsAuth, '--data-urlencode',
+        """json='{"parameter": []}'"""
+      ]
+
+      // println "*"*40
+      // println cmd.join(' ')
+      // println "*"*40
+
+      def process = cmd.execute()
+		  def stdout = new StringWriter()
+		  def stderr = new StringWriter()
+
+      process.consumeProcessOutput(stdout, stderr)
+
+      def exitCode = process.waitFor()
+      if ( exitCode == 0 )
+      {
+          render contentType: 'application/json', text: stdout.toString()
+      }
+      else
+      {
+        render contentType: 'text/plain', text: stderr.toString()
+      }
 
     }
 }
